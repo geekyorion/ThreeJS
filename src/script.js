42114f9b443c885a9ec3164d8b913f4b1ecaf5f0
Shashank Sharma <shashank.ns67@gmail.com>
2022-06-13 17:46:44 +0530

    added cameras

--- src/script.js
+++ src/script.js
@@ -2,6 +2,18 @@ import * as THREE from 'three';
 import gsap from 'gsap';
 import './style.css';
 
+/**
+ * Camera: an abstract class which is inherited by every type of camera
+ * Types of Camera:
+ *   1. Array Camera: to render multiple views [can use to split the scene view for various players]
+ *      It uses multiple cameras to render the scene
+ *   2. Cube Camera: to render 6 sides like cubes [used for environment maps]
+ *   3. Stereo Camera: to create the parallax effect, it renders the scene using
+ *      two cameras to mimic the eye views.
+ *   4. Orthographic Camera: Renders without perspective
+ *   5. Perspective Camera: Renders with perspective
+ */
+
 // Scene
 const scene = new THREE.Scene();
 
@@ -14,9 +26,38 @@ scene.add(box);
 // Sizes
 const sizes = { width: 800, height: 600 };
 
-// Camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
+// Perspective Camera [any object that are closer than near and further than far will not be rendered]
+const camera = new THREE.PerspectiveCamera(
+  75, // FOV: field of view (vertical angle in degrees)
+  sizes.width / sizes.height, // aspect ratio
+  0.1,  // how near can camera look
+  100 // how far can camera look
+);
+
+// Orthographic Camera: [left, right, top, bottom for rectangle render]
+/**
+ * this camera renders and resize everything as per the size
+ * try changing the height in sizes. to overcome this problem,
+ * we can use AspectRatio in orthographic camera too
+ */
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio, // left
+//   1 * aspectRatio,  // right
+//   1,  // top
+//   -1, // bottom
+//   0.1,  // near
+//   100 // far
+// );
+/**
+ * Note: if we put (1, -1, 1, -1) then it will render the mirror of the scene.
+ * also we need to multiply apsectRation to horizontal values [left and right]
+ * to render the perfect square because we are calculating aspectRation using width/height
+ */
+
+camera.position.y = 2;
 camera.position.z = 5;
+camera.lookAt(box.position);
 scene.add(camera);
 
 // Renderer
